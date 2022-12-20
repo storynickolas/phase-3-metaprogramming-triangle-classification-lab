008aa0466ef8224ee589a6fab33574ca9d1b377f
Nick Story <storynickolasl@gmail.com>
2022-12-20 07:10:52 -0500

    commit tree b60983b7ea5b086b753072a1d94a070d23e05725

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    cat = self.valid
+    if cat && @l1 == @l2 && @l2 == @l3
+      :equilateral
+    elsif cat && @l1 == @l2 || cat && @l1 == @l3 || cat && @l2 == @l3
+      :isosceles
+    elsif cat
+      :scalene
+    else
+      raise TriangleError
+    end
+  end
+
+  private
+
+  def valid
+    if @l1 != 0 && @l2 != 0 && @l3 != 0 && @l1 + @l2 > @l3 && @l2 + @l3 > @l1 && @l1 + @l3 > @l2
+      true
+    else
+      false
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+    puts 'Wrong'
+  end
+
+
 end
+
